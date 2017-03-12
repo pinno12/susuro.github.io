416eea984614cbd376b60bb57128cf110c97dd02
Won <pinno12@kaist.ac.kr>
2017-03-12 21:13:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@ $ds = "3";
 
 ?>
 
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
+  <head>
+     <meta charset="utf-8">
+<!--  <link rel="stylesheet" type="text/css" href="C:\Users\OH Jame\Desktop\style.css"> -->
+</head>
+  
   ohoh
   <?php
   
